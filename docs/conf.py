c15b4fcc07449b420a45841dcbffabd617932328
Zack Lovatt <zack@zacklovatt.com>
2017-08-25 10:28:08 -0700

    Tweaks conf to match jsdocs; removes redundant comment from index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -23,23 +23,25 @@ release = u'0.0.1'
 
 pygments_style = 'sphinx'
 
+highlight_language = "javascript"
+
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {
-    'sidebarwidth': '20rem',
+# html_theme_options = {
+    # 'sidebarwidth': '20rem',
     # 'cssfiles': ['_static/extra.css']
-}
+# }
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "After Effects Scripting Guide"
+# html_title = "After Effects Scripting Guide"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Home"
+# html_short_title = "Home"
 
 # html_logo = None
 
@@ -52,24 +54,24 @@ html_static_path = ['_static']
 # html_last_updated_fmt = ''
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    '**': ['searchbox.html', 'localtoc.html', 'globaltoc.html'],
-    'index': ['searchbox.html']
-}
+# html_sidebars = {
+#     '**': ['searchbox.html', 'localtoc.html', 'globaltoc.html'],
+#     'index': ['searchbox.html']
+# }
 
 # If true, links to the reST sources are added to the pages.
 # html_show_sourcelink = True
 
-html_show_sphinx = True
+# html_show_sphinx = True
 
-html_show_copyright = True
+# html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
 # html_use_opensearch = ''
 
-html_search_language = 'en'
+# html_search_language = 'en'
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
@@ -77,6 +79,3 @@ html_search_language = 'en'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ae-scripting-guide'
-
-extensions = [
-]
--- docs/index.rst
+++ docs/index.rst
@@ -1,8 +1,3 @@
-.. aedocs documentation master file, created by
-   sphinx-quickstart on Mon Feb  1 14:43:26 2016.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to the After Effects Scripting Guide!
 ---------------------------------------------
 
